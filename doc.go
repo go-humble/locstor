c483d219cb926ddb6e2e9b1be90422f609637ec1
Alex Browne <stephenalexbrowne@gmail.com>
2015-09-12 19:51:08 -0700

    Add version and link to repo in doc comment

--- doc.go
+++ doc.go
@@ -5,4 +5,9 @@
 // Package locstor provides gopherjs bindings for the localStorage API. It
 // allows you to store and retreive any arbitrary go data structure, and is
 // intended to be compiled to javascript with gopherjs and run in the browser.
+//
+// Version X.X.X (develop)
+//
+// For the full source code, example usage, and more information visit
+// https://github.com/go-humble/locstor.
 package locstor
